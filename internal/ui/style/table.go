d1f31927d18ca5c8227076a769a1af752213a764
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-06-13 21:25:04 +0300

    draft

--- internal/ui/style/table.go
+++ internal/ui/style/table.go
@@ -8,17 +8,20 @@ import (
 	"github.com/rivo/tview"
 )
 
+// Table ...
 type Table struct {
 	Object *tview.Table
 	title  string
 }
 
+// NewTable return custom Table object with predifined styles.
 func NewTable() *Table {
 	return &Table{
 		Object: tview.NewTable(),
 	}
 }
 
+// WithTitle add title to table.
 func (t *Table) WithTitle(title string) *Table {
 	t.title = title
 	t.Object.
@@ -27,17 +30,18 @@ func (t *Table) WithTitle(title string) *Table {
 	return t
 }
 
+// WithCount add to table title count of rows.
 func (t *Table) WithCount(count int) *Table {
-	if t.title == "" {
-		t.Object.
-			SetTitle(fmt.Sprintf("[%d]", count))
-		return t
+	switch {
+	case t.title == "":
+		t.Object.SetTitle(fmt.Sprintf("[%d]", count))
+	default:
+		t.Object.SetTitle(fmt.Sprintf(" %s [red][%d][white] ", t.title, count))
 	}
-	t.Object.
-		SetTitle(fmt.Sprintf(" %s [red][%d][white] ", t.title, count))
 	return t
 }
 
+// AsContent add custom styles for table.
 func (t *Table) AsContent() *Table {
 	t.Object.
 		SetSelectable(true, false).
@@ -47,26 +51,32 @@ func (t *Table) AsContent() *Table {
 	return t
 }
 
+// AddCellContent add item [row, col] as table value with Content color style.
 func (t *Table) AddCellContent(r, c int, value string) {
 	t.addCell(r, c, value, ColorContent)
 }
 
+// AddCellSecondary add item [row, col] as table value with Secondary color style.
 func (t *Table) AddCellSecondary(r, c int, value string) {
 	t.addCell(r, c, value, ColorSecondary)
 }
 
+// AddCellPrimary add item [row, col] as table value with Primary color style.
 func (t *Table) AddCellPrimary(r, c int, value string) {
 	t.addCell(r, c, value, ColorPrimary)
 }
 
+// AddCellTitle add item [row, col] as table value with Title color style.
 func (t *Table) AddCellTitle(r, c int, value string) {
 	t.addCell(r, c, value, ColorTitle)
 }
 
+// AddCellText add item [row, col] as table value with Default color style.
 func (t *Table) AddCellText(r, c int, value string) {
 	t.addCell(r, c, value, ColorText)
 }
 
+// AddCellHeader add item [row, col] as table Header.
 func (t *Table) AddCellHeader(r, c int, value string) {
 	t.Object.SetCell(
 		r,
