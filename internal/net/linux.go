076fd025eaef3ef840551fde7f8947c1ca205ea9
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-06-14 19:39:41 +0300

    draft

--- internal/net/linux.go
+++ internal/net/linux.go
@@ -65,8 +65,72 @@ func scan() ([]network, error) {
 }
 
 func stat() (*network, error) {
-	nn := randomNetwork()
-	return &nn, nil
+
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to system bus: %v", err)
+	}
+
+	nm := conn.Object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+	var activeConnections []dbus.ObjectPath
+	err = nm.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager", "ActiveConnections").Store(&activeConnections)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get active connections: %v", err)
+	}
+
+	for _, connPath := range activeConnections {
+		connObj := conn.Object("org.freedesktop.NetworkManager", connPath)
+		var devices []dbus.ObjectPath
+		err = connObj.Call("org.freedesktop.NetworkManager.Connection.Active.GetDevices", 0).Store(&devices)
+		if err != nil {
+			continue
+		}
+
+		for _, devicePath := range devices {
+			deviceObj := conn.Object("org.freedesktop.NetworkManager", devicePath)
+			var deviceType uint32
+			err = deviceObj.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.Device", "DeviceType").Store(&deviceType)
+			if err != nil {
+				continue
+			}
+
+			if deviceType == 2 { // 2 is the type for Wi-Fi devices
+				var apPath dbus.ObjectPath
+				err = deviceObj.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.Device.Wireless", "ActiveAccessPoint").Store(&apPath)
+				if err != nil || apPath == "/" {
+					continue
+				}
+
+				apObj := conn.Object("org.freedesktop.NetworkManager", apPath)
+
+				var ssid []byte
+				err = apObj.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.AccessPoint", "Ssid").Store(&ssid)
+				if err != nil {
+					continue
+				}
+
+				var freq uint32
+				err = apObj.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.AccessPoint", "Frequency").Store(&freq)
+				if err != nil {
+					continue
+				}
+
+				var strength uint8
+				err = apObj.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.AccessPoint", "Strength").Store(&strength)
+				if err != nil {
+					continue
+				}
+
+				return &network{
+					ssid:  string(ssid),
+					freq:  fmt.Sprintf("%d MHz", freq),
+					level: string(strength),
+				}, nil
+			}
+		}
+	}
+
+	return nil, nil
 }
 
 func randomNetwork() network {
