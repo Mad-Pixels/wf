81c3e04dc4f85a19ae3d8ddc1cf5c0180f54f3b9
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-06-16 22:53:39 +0300

    draft

--- internal/ui/app.go
+++ internal/ui/app.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/Mad-Pixels/wf/internal/ui/binding"
@@ -66,25 +67,85 @@ func (u *ui) ShowModal(data binding.TriggerModalData) {
 	//		AddItem(nil, 1, 1, false), 0, 1, true).
 	//	AddItem(nil, 1, 1, false)
 
-	flex := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(nil, 0, 1, false). // Пустой элемент для выравнивания по вертикали
-		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
-			AddItem(nil, 0, 1, false). // Пустой элемент для выравнивания по горизонтали
-			AddItem(data.P, 60, 1, true).
-			AddItem(nil, 0, 1, false), 10, 1, true). // Пустой элемент для выравнивания по горизонтали
-		AddItem(nil, 0, 1, false)
+	///
+	// flex := tview.NewFlex().
+	// 	SetDirection(tview.FlexRow).
+	// 	AddItem(nil, 0, 1, false). // Пустой элемент для выравнивания по вертикали
+	// 	AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+	// 		AddItem(nil, 0, 1, false). // Пустой элемент для выравнивания по горизонтали
+	// 		AddItem(data.P, 60, 1, true).
+	// 		AddItem(nil, 0, 1, false), 10, 1, true). // Пустой элемент для выравнивания по горизонтали
+	// 	AddItem(nil, 0, 1, false)
 
-	u.pages.AddPage("modal", flex, true, false)
-	//container := tview.NewPages().
-	//	AddPage("background", u.pages, true, true).
-	//	AddPage("modal", page, true, true)
-	u.pages.ShowPage("modal")
-	u.app.SetFocus(data.P)
+	// u.pages.AddPage("modal", flex, true, false)
+	// //container := tview.NewPages().
+	// //	AddPage("background", u.pages, true, true).
+	// //	AddPage("modal", page, true, true)
+	// u.pages.ShowPage("modal")
+	// u.app.SetFocus(data.P)
+	///
 
 	//u.pages.AddPage("modal", container, false, true).ShowPage("modal")
 	//u.app.SetFocus(data.P)
 	//u.app.SetFocus(form)
+
+	// modal := func(p tview.Primitive, width, height int) tview.Primitive {
+	// 	return tview.NewGrid().
+	// 		SetColumns(0, width, 0).
+	// 		SetRows(0, height, 0).
+	// 		AddItem(p, 1, 1, 1, 1, 0, 0, true)
+	// }
+
+	// modal := func(p tview.Primitive, width, height int) tview.Primitive {
+	// 	ooo := tview.NewFlex().
+	// 		AddItem(nil, 0, 1, false).
+	// 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+	// 			AddItem(nil, 0, 1, false).
+	// 			AddItem(data.P, 0, 1, false).
+	// 			//AddItem(tview.NewFlex().AddItem(data.P, 0, 1, false).SetBorder(true), height, 1, true).
+	// 			AddItem(nil, 0, 1, false), width, 1, true).
+	// 		AddItem(nil, 0, 1, false)
+	// 	return ooo
+	// }
+
+	// box := tview.NewBox().
+	// 	SetBorder(true).
+	// 	SetTitle("Centered Box")
+
+	// mm := modal(box, 40, 10)
+
+	// u.pages.AddPage("modal", mm, true, true)
+	// u.app.SetRoot(u.pages, true).Draw().SetFocus(mm)
+
+	content := tview.NewFlex().
+		AddItem(nil, 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(nil, 0, 1, false).
+			AddItem(primitive("title", data.P), 9, 1, true).
+			AddItem(nil, 0, 1, false),
+			34, 1, true).
+		AddItem(nil, 0, 1, false)
+
+	container := tview.NewPages().
+		AddPage("main", u.pages.ShowPage("main"), true, true).
+		AddPage("modal", content, true, true)
+
+	u.app.SetRoot(container, true) //.Draw()
+}
+
+func primitive(name string, p tview.Primitive) *tview.Flex {
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(p, 0, 1, true)
+
+	flex.SetBorder(true)
+	flex.SetBorderColor(tcell.ColorDodgerBlue)
+
+	flex.SetTitleColor(tcell.ColorOrangeRed)
+	if name != "" {
+		flex.SetTitle(fmt.Sprintf(" %s ", name))
+	}
+	return flex
 }
 
 func (u *ui) Draw() {
