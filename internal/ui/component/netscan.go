5187e4f26aa20806a4599e49fbe4da2d67033f1e
mr-chelyshkin <argent-keel-0z@icloud.com>
2024-06-18 16:37:37 +0300

    draft

--- internal/ui/component/netscan.go
+++ internal/ui/component/netscan.go
@@ -84,7 +84,7 @@ func NetScan(render RenderInterface, logger LoggerInterface, view ViewInterface)
 						return self.networks[selectedRow-1].GetSsid()
 					}(),
 					func(ssid string) {
-						err := net.NewNetwork().Conn(context.Background(), ssid, "Tislam55")
+						err := net.NewNetwork().Conn(context.Background(), ssid, "qwerty")
 						if err != nil {
 							self.WriteMsg(err.Error())
 						}
